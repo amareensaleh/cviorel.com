03f57d600e1700ccfdf3d7d26d1572db497acf15
amarees <saleh.amareen@dominos.com>
2025-03-18 23:29:25 -0400

    just build it

--- alwayson-availability-groups-using-containers/build.sh
+++ alwayson-availability-groups-using-containers/build.sh
@@ -2,8 +2,5 @@
 
 rm -rf build.log
 
-# Regular build
-#docker build -t mssql2019-custom -f linux/dockerfile . > build.log 2>&1
 
-# Using emulation since apple M3 is arm64
-docker buildx build --no-cache --platform linux/amd64 -t mssql2019-custom -f linux/dockerfile .
+docker build -t mssql2019-custom -f linux/dockerfile . > build.log 2>&1
