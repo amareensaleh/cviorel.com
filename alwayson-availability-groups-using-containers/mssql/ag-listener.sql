a1d6ae138ddc741028e9b5b4eb2a5b6f0598a9e8
amarees <saleh.amareen@dominos.com>
2025-03-19 15:13:10 -0400

    Added SQL to create the ag listener.

---
+++ alwayson-availability-groups-using-containers/mssql/ag-listener.sql
@@ -0,0 +1,19 @@
+
+:SETVAR node_01 "sql_node_01"
+:SETVAR node_02 "sql_node_02"
+:SETVAR node_03 "sql_node_03"
+:SETVAR sa_user "sa"
+
+:r /usr/config/miscpassword.env
+
+/*
+create login, master key and certificate on primary
+*/
+:CONNECT $(node_01) -U $(sa_user) -P $(sa_password)
+USE master
+GO
+
+ALTER AVAILABILITY GROUP [AG1]
+    ADD LISTENER 'AG1_Listener' (WITH IP(('172.16.238.30', '255.255.255.0')), PORT = 1433);
+GO
+
