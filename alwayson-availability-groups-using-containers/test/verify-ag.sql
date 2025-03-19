b1af237413b2da2f3b53080c75961e0038bbec3e
amarees <saleh.amareen@dominos.com>
2025-03-19 15:13:47 -0400

    Added more statements to verify ag group and listener.

--- alwayson-availability-groups-using-containers/test/verify-ag.sql
+++ alwayson-availability-groups-using-containers/test/verify-ag.sql
@@ -1,4 +1,4 @@
-SELECT *
+SELECT resource_group_id
 FROM sys.availability_groups;
 
 SELECT *
@@ -8,32 +8,37 @@ SELECT *
 FROM sys.dm_hadr_database_replica_states
 
 SELECT *
+FROM sys.dm_hadr_availability_replica_states
+
+SELECT *
 FROM sys.dm_xe_sessions
 WHERE name = 'AlwaysOn_health';
+
+-- Check availability group listeners
+select *
+from sys.availability_group_listener_ip_addresses
 
 SELECT *
 FROM sys.availability_group_listeners;
 
-SELECT ag.name, ar.replica_server_name, ars.role_desc, ars.synchronization_health_desc
+SELECT *
+FROM sys.dm_tcp_listener_states;
+
+-- Check availability group health and roles
+SELECT distinct ag.name as vailability_group_name, ar.replica_server_name replica_server_name, ars.role_desc as role, drs.synchronization_state_desc as sync, ars.synchronization_health_desc as health
 FROM sys.dm_hadr_availability_replica_states ars
          JOIN sys.availability_replicas ar ON ars.replica_id = ar.replica_id
+         JOIN sys.dm_hadr_database_replica_states drs ON ars.replica_id = ar.replica_id
          JOIN sys.availability_groups ag ON ar.group_id = ag.group_id;
 
+-- Check HA endpoint
 SELECT name, state_desc, is_encryption_enabled, type_desc
 FROM sys.database_mirroring_endpoints;
 
-SELECT * FROM sys.tcp_endpoints WHERE name = 'Hadr_endpoint';
+SELECT * FROM sys.tcp_endpoints as tcpe WHERE name = 'Hadr_endpoint';
 
-SELECT * FROM sys.certificates WHERE name = 'dbm_certificate';
+-- check certificates
+SELECT * FROM sys.certificates
 
-SELECT database_name FROM sys.availability_databases_cluster;
-
-SELECT group_id, replica_id, * FROM sys.availability_replicas;
-
-SELECT *
-FROM sys.dm_hadr_database_replica_states;
-
-SELECT ag.name, ar.replica_server_name, ars.role_desc, ars.synchronization_health_desc
-FROM sys.dm_hadr_availability_replica_states ars
-         JOIN sys.availability_replicas ar ON ars.replica_id = ar.replica_id
-         JOIN sys.availability_groups ag ON ar.group_id = ag.group_id;
+-- Check availability database
+SELECT * FROM sys.availability_databases_cluster;
